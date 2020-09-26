e6dc676380d7c89c4a5703dadabf3a6db1191f59
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-26 12:34:32 +0530

    Second

--- WageComp/Program.cs
+++ WageComp/Program.cs
@@ -21,7 +21,7 @@ namespace WageComp
                 empHrs = 0;
 
             }
-            Console.WriteLine("The wage is " + empHrs * wagePerHour);
+            Console.WriteLine("The Wage is " + empHrs * wagePerHour);
 
         }
     }
