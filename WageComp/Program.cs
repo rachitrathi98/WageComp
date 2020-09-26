042b894b9dea1e2af268cd7df5b40c220676c298
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-26 11:45:50 +0530

    First Commit

---
+++ WageComp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WageComp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int check = r.Next(0, 2);
+            if (check == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            
+
+        }
+    }
+}
