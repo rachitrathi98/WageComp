1fc01e6d155cc924b5c884cdfedadbe5fd22cd78
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-27 19:01:40 +0530

    First Commit

--- WageComp/Program.cs
+++ WageComp/Program.cs
@@ -21,7 +21,7 @@ namespace WageComp
                 empHrs = 0;
 
             }
-            Console.WriteLine("The Wage is not so good third" + empHrs * wagePerHour);
+            Console.WriteLine("The daily wage is " + empHrs * wagePerHour);
 
         }
     }
