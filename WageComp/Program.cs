91d13193dbfb82b951cd29756a556554ad5e2f2f
Rachit Rathi <rachitrathi98@gmail.com>
2020-09-27 19:14:26 +0530

    First Commit

--- WageComp/Program.cs
+++ WageComp/Program.cs
@@ -10,16 +10,20 @@ namespace WageComp
             int empHrs;
             Random r = new Random();
 
-            int check = r.Next(0, 2);
+            int check = r.Next(0, 3);
             if (check == 1)
             {
                 empHrs = 8;
 
             }
+            else if(check==2)
+            {
+                empHrs = 4;
+
+            }
             else
             {
                 empHrs = 0;
-
             }
             Console.WriteLine("The daily wage is " + empHrs * wagePerHour);
 
