280bf308b8e4c8a7f46ad2813ef7e4e76f06204f
Rachit Rathi <rachitrathi98@gmail.com>
2020-10-06 08:53:04 +0530

    First Commit

--- WageComp/ComputeWage.cs
+++ WageComp/ComputeWage.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace WageComp
 {
-    class ComputeWage
+    public class ComputeWage
     {
+        public string company;
+        public int ratePerHours;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public ComputeWage(string company, int ratePerHours, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.ratePerHours = ratePerHours;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.totalEmpWage = 0;
+        }
+
+        public void SettotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string toString()
+        {
+            return "Total Emp Wage for company: " + this.company + " is " + this.totalEmpWage;
+        }
     }
+
 }
